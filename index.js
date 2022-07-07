078f4b042bf0a909510bc20eac89c73175ed3602
DavidShulga <david-shulga@mail.ru>
2022-07-07 22:02:29 +0300

    backend update

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => console.log("Server started on port" + " " + PORT));
+app.listen(PORT, () => console.log("Server started on port" + "  " + PORT));
